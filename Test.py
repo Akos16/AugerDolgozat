202e6d73483d81e577169ccfa634ab98650608b3
Akos16 <denkingerakos2003@gmail.com>
2026-01-26 23:18:04 +0100

    Add std, skewness, kurtosis

--- Test.py
+++ Test.py
@@ -1,9 +1,26 @@
-# Python Program illustrating 
-# numpy.mean() method 
 import numpy as np
-  
-# 1D array 
-arr = [5,1,4,5,5]
+import math
+from scipy.stats import skew
+from scipy.stats import gumbel_r
 
-print("arr : ", arr) 
-print("mean of arr : ", np.mean(arr))
+#gumbel adatok
+x1 = gumbel_r.rvs(loc=800, scale=40, size=1000000)
+n = len(x1)
+xMean = np.mean(x1);
+#szoras, skewness, kurtosis
+mtwo = mthree = mfour = m2 = m3 = m4 = 0
+m4 = 0
+for i in x1:
+    mtwo += math.pow(i - xMean, 2)
+    mthree += math.pow(i - xMean, 3)
+    mfour += math.pow(i - xMean, 4)
+m2 = mtwo / n
+m3 = mthree / n
+m4 = mfour / n
+
+#Fisher-Pearson coefficient of skewness
+g1 = m3 / (m2 ** 1.5)
+print("Fisher-Pearson coefficient of skewness: ", g1)
+#adjusted Fisher-Pearson coefficient of skewness
+G1 = math.sqrt(n * (n - 1)) / (n-2) * g1
+print("adjusted Fisher-Pearson coefficient of skewness", G1)
